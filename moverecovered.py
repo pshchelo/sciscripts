9d983e28658320cf1329dca6f5c8aed29712f2bd
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2012-05-29 19:35:03 +0200

    changed moverecovered.py to accept a specific file extension

--- moverecovered.py
+++ moverecovered.py
@@ -1,11 +1,13 @@
 import os
+import sys
 import glob
 
-fnames = glob.iglob("*.tif")
+ext = sys.argv[1]
+fnames = glob.iglob("*.%s"%ext)
 j=0
 print "Start renaming..."
 for f in fnames:
-    dest = os.path.join('m:/recovery/images/tif', f)
+    dest = os.path.join('../recovery/%s'%ext, f)
     os.renames(f, dest)
     j += 1
     if j%100==0:
