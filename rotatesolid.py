38113f0c54e52f162cf64220f643aaf0d196c2b0
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2013-01-17 22:54:07 +0100

    changed volume calculation to 0-division-free version

--- rotatesolid.py
+++ rotatesolid.py
@@ -3,25 +3,17 @@ from __future__ import division
 import numpy as np 
 
 def volume_trapz_rotx(y, x):
-    """Calculate vulume of body of rotation around X-axis.
-
-    Subsequent points can have the same x-coordinate,
-    in this case the volume between them would be NaN
-    due to 0-devision, but then NaN is substituted for 0,
-    making no contribution to the integral.
+    """Volume of solid of rotation around X-axis.
     
     """
     dx = np.diff(x)
     dy = np.diff(y)
     x = x[:-1]
     y = y[:-1]
-    k = dy/dx # in case of vertically aligned points
-    b = y - x*k
-    # integrated square of the radius of revolution for trapezoid
-    r2integrated = lambda t: k*k*t*t*t/3 + k*b*t*t + b*b*t
-    dv = np.pi*(r2integrated(x+dx)-r2integrated(x))
-    dv = np.nan_to_num(dv)
-    return np.sum(dv)
+    # taking each slice as a truncated cone with radii y and y+dy and height dx
+    dV = np.pi/3 * np.abs(dx) * (y**2 + (y+dy)**2 + y*(y+dy))
+    return sum(dV)
+
 def surface_trapz_rotx(y, x):
     """Surface of solid of rotation around X axis.
 
